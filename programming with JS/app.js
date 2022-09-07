91ceb240d2af934c425c130d9ffd8776a80c83c4
HassanNadeemWebD <hn178139@gmail.com>
2022-09-07 12:33:16 +0500

    update

--- programming with JS/app.js
+++ programming with JS/app.js
@@ -26,16 +26,16 @@
 // length 1) and returns true if it is a vowel, false otherwise
 
 
-let userInput;
-userInput = prompt("Enter Any Three Digit Number");
-console.log(userInput.length);
-if(userInput == 'a' || userInput == 'e' || userInput == 'i' || userInput == 'o' || userInput == 'u' )
-{
-   console.log(userInput + " is a vowel") 
-}
-else{
-    console.log(userInput + " is not a vowel") 
-}
+// let userInput;
+// userInput = prompt("Enter Any Three Digit Number");
+// console.log(userInput.length);
+// if(userInput == 'a' || userInput == 'e' || userInput == 'i' || userInput == 'o' || userInput == 'u' )
+// {
+//    console.log(userInput + " is a vowel") 
+// }
+// else{
+//     console.log(userInput + " is not a vowel") 
+// }
 
 // if (userInput.length <= 3) {
 //     if (userInput > 0) {
@@ -60,3 +60,53 @@ else{
 // console.log("Your Input Having " + userInput.length + " Characters which is Invalid")
 
 // }
+
+// let arr = [1,2,3,4, "Apple" , "mango" , true , null];
+// let userInput = prompt("Input any Word Match or Enter Zero");
+// if(userInput == 0)
+// {
+// let userInput2 = parseInt(prompt("Input any Number Match"));
+// console.log("The Index of Your Input Number is " + arr.indexOf(userInput2));
+
+
+// }else{
+
+// console.log("The Index of Your Input Word is " + arr.indexOf(userInput));
+
+// }
+// console.log(userInput)
+// arr.push("abc")
+// arr.push( userInput)
+// console.log(arr)
+// console.log(arr.indexOf(userInput))
+// console.log(typeof userInput)
+
+let obj = {
+    name:"abc" ,
+    contact: "0348236583",
+    passORfail: true,
+    hobbies: [1, 2, 3, 4, "Apple", "mango", true, null],
+    bio: {
+        fatherName: "def",
+        gender: "male",
+        siblings: ["xy", "Hiii", "kl"]
+    }
+}
+
+console.log(obj)
+document.write(JSON.stringify(obj));
+let json = JSON.stringify(obj);
+
+function myfunc() {
+    let json = JSON.stringify(obj);
+
+    document.getElementById("myjson").innerHTML = obj.bio.siblings[1];
+
+}
+for (let i = 0;  i <=100 ; i++) {
+    console.log(i);
+    
+}
+// document.getElementById('myjson').innerHTML = json;
+
+
