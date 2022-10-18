c91b85d1e59f7306e484527f890e9343402d589a
HassanNadeemWebD <hn178139@gmail.com>
2022-10-18 10:18:45 +0500

    commit

---
+++ programming with JS/devtools.js
@@ -0,0 +1,14 @@
+console.log("DevTools")
+let name = "hassan"
+let a;
+for(a= 0; a<=10; a++)
+{
+    console.log(a)
+}
+// alert("Hello")
+// console.log(a)
+let myFunc= ()=>{
+    document.getElementById("id").innerHTML = " Aptech"
+}
+
+myFunc();
